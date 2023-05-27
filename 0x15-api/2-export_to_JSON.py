9fd2e9d1a11553ef5e736a5df0f993d167757e2b
Donald Peters <donny9585@gmail.com>
2023-05-27 20:31:37 +0100

    Done task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,6 +4,7 @@ This script uses a REST API for a given employee ID,
 returns information about his/her TO-DO list progress.
 """
 import csv
+import json
 
 import requests
 import sys
@@ -23,16 +24,21 @@ def employee_todo_progress(employee_id):
     todo_url = '{}/todos/?userId={}'.format(url, employee_id)
     response = requests.get(todo_url)
     todo_data = response.json()
-    # print(todo_data)
 
-    with open('{}.csv'.format(employee_id), 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
+    task_list = []
+    for task in todo_data:
+        task_dict = task.copy()
+        task_dict.pop('userId')
+        task_dict.pop('id')
+        task_dict['task'] = task_dict['title']
+        del task_dict['title']
+        task_dict['username'] = employee_data.get('username')
+        task_list.append(task_dict)
 
-        for task in todo_data:
-            csv_list = [employee_id, employee_data.get("username"),
-                        task.get('completed'), task.get('title')]
+    json_data = {employee_id: task_list}
 
-            csv_writer.writerow(csv_list)
+    with open('{}.json'.format(employee_id), 'w', ) as json_file:
+        json.dump(json_data, json_file)
 
 
 if __name__ == "__main__":
