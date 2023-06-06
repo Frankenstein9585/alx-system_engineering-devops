535543fa1148eb2efeda88b254614daee5909aa0
Frankenstein9585 <donny9585@gmail.com>
2023-06-06 22:36:50 +0100

    Modified task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,24 +3,27 @@
 import requests
 
 
-def recurse(subreddit):
+def recurse(subreddit, hot_list=None, after=None):
     """Returns the hot posts
     for a given subreddit recursively"""
+    if hot_list is None:
+        hot_list = []
+
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'Custom User Agent'}
+    params = {'after': after} if after else None
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-        hot_list = []
         for child in data['data']['children']:
             hot_list.append(child['data']['title'])
 
         after = data['data']['after']
 
         if after:
-            return recurse(subreddit)
+            return recurse(subreddit, hot_list, after=after)
         else:
             return hot_list
     else:
