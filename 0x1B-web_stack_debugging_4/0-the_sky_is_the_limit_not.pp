23d9a5adad5feec392874d96019154335c130123
Frankenstein9585 <donny9585@gmail.com>
2023-06-15 13:18:02 +0100

    Done task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Remove errors (I still don't like DevOps)
+exec { 'file limit':
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  provider => shell,
+}
